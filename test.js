b80b2cfa8de8d836c13e697a387a52cd13e3bfdd
Maiihuy73 <106956787+Maiihuy73@users.noreply.github.com>
2022-06-09 13:48:40 +0700

    Add files via upload

--- test.js
+++ test.js
@@ -1,8 +1,18 @@
-<!DOCTYPE html>
-<html>
-<body>
-<h1> What can JavaScript Do?</h1>
-<p id="demo"> Tôi tên là Mai Huy.</p>
-<button></button>
-</body>
-</html>
+//scope: Phạm vi
+/*
+- Golbal: toàn cầu
+-Code block- khối mã: let, const
+-Local scope- Hàm: var, function
+*/
+function logger(){
+    var fullname = 'Mai Quoc Huy'
+    console.log(logger)  
+ }
+ logger(fullname) 
+ // Khi gọi mỗi hàm luôn có 1 phạm vi mới được tạo
+ function logger(first, last){
+     console.log(first, last)
+ }
+ logger(' Huy', 'Mai')
+ logger('Long''Nguyen')
+ logger('Y','Nguyeen')
